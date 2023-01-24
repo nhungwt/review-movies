873406679d4d2e22bbb4b5e539efbbc9dd2eba3a
nhungwt <nhunghongwt@gmail.com>
2023-01-24 16:27:16 +0700

    Create model Movie the first time

--- movie/models.py
+++ movie/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
 # Create your models here.
+
+
+class Movie(models.Model):
+    title = models.CharField(max_length=200, blank=True)
+    infor = models.CharField(max_length=500) # contain link wiki
+    publish = models.CharField(max_length=4, blank=False)
+    updated = models.DateField(auto_now=True)
+    img = models.CharField(max_length=500)
+    des = models.CharField(max_length=200)
